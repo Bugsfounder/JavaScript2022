fe11ebb7865a385517205b1a6c44cc6d5f61e172
Bugs Founder <bugsfounder2021@gmail.com>
2022-01-21 04:50:28 +0530

    Console Logs, Errors, Warnings, Tables & More | JavaScript Tutorial In Hindi #2

---
+++ js/index.js
@@ -0,0 +1,25 @@
+console.time("Your Code Took: ");
+console.log("Hello Manisha");
+console.log(4 + 34);
+console.log(34);
+console.log(true);
+console.log([1, 34, 5, 3]);
+console.log({ name: "Manisha", Language: "JavaScript" });
+console.table({ name: "Manisha", Language: "JavaScript", Class: 10 });
+console.warn("This is a Warning");
+// console.clear();
+console.timeEnd("Your Code Took: ");
+// console.assert(566 < 189, "Age > 189 is not possible");
+console.error("This is an error");
+
+
+
+// This is a single line comment
+
+/*
+    This 
+    is a
+    multi 
+    line 
+    commet"Console Logs, Errors, Warnings & More | JavaScript Tutorial In Hindi #2
+*/
