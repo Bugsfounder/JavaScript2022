299b80b6992f522c0607135f5165973534e88894
Bugs Founder <bugsfounder2021@gmail.com>
2022-01-21 06:44:22 +0530

    Data Types in JavaScript (Primitive & Reference Types) | JavaScript Tutorial In Hindi #4

---
+++ js/4_DataTypes.js
@@ -0,0 +1,55 @@
+console.log('JavaScript Data Types Tutorial');
+
+// PRIMITIVE DATATYPES
+// STRING
+let myName = "Manisha";
+console.log('This is String:', myName);
+console.log('My DataType:', typeof myName);
+
+// NUMBERS
+let marks = 34;
+console.log('This is Number:', marks);
+console.log('My DataType:', typeof marks);
+
+// BOOLEAN
+let isDriver = true;
+console.log('This is Boolean:', isDriver);
+console.log('My DataType:', typeof isDriver);
+
+// NULL
+let nullVar = null;
+console.log('This is Null:', nullVar);
+console.log('My DataTypes:', typeof nullVar);
+
+// UNDEFINED
+let undef = undefined;
+console.log('This is Undefined:', undef);
+console.log('My DataTypes:', typeof undef);
+
+// REFERENCE DATATYPES ==> YOU CAN STORE ANY PRIMITIVE DATA TYPES IN REFERENCE DATATYPES
+
+// ARRAYS
+let myArr = [1, 2, 3, 4, 5, false, 'string', undefined, null];
+console.log('This is Array:', myArr);
+console.log('My DataTypes:', typeof myArr);
+
+// OBJECT LITERALS
+let marksObj = {
+    harry: 100,
+    manisha: 99,
+    Sbubham: 98
+};
+console.log('This is Object Literals:', marksObj);
+console.log('My DataTypes:', typeof marksObj);
+
+// FUNCTIONS
+function findName() {
+
+}
+console.log('This is Functions:', findName);
+console.log('My DataTypes:', typeof findName);
+
+// DATES
+let date = new Date();
+console.log('This is Date:', date);
+console.log('My DataTypes:', typeof date);
