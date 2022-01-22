bb9b33473aa1d89223a3bbf16d946d916f1cd216
Bugs Founder <bugsfounder2021@gmail.com>
2022-01-22 16:20:12 +0530

    Manipulating Websites Using JS Window Object | JavaScript Tutorial In Hindi #11

---
+++ js/11_ManipulatingWebsitesUsingJSWindowObject.js
@@ -0,0 +1,22 @@
+console.log("JavaScript Manipulating Websites Using JS Window Object Tutorial");
+// DOM - Document Object Module 
+
+let a = window;
+// window.alert("Hello Manisha");
+// alert("Hello Manisha");
+// a = prompt("This is destroy your computer. Enter Your Name.");
+// a = confirm("Are You Sure You Want to Delete this");
+
+// a = window.document; 
+// a = window.innerHeight;
+// a = window.innerWidth;
+a = scrollX
+a = scrollY
+a = window.location;
+// a = window.location.pathname; // to get present pathname (slug)
+a = location.toString();
+a = history;
+// history.go(-1);
+
+
+console.log(a);
