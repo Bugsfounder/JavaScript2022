51bc5103d65cb49b306e25f36b92b156f3e5de51
Bugs Founder <bugsfounder2021@gmail.com>
2022-01-23 15:23:53 +0530

    Smart Page Crawler In JS Exercise1: Solution | JavaScript Tutorial In Hindi #19

--- exercises/1_WebPageCrawlerInJavaScript-Exercise.js
+++ exercises/1_WebPageCrawlerInJavaScript-Exercise.js
@@ -1,17 +1,11 @@
 // Exercise 1
 // YOU HAVE TO CREATE A VARIABLE WHICH IS A STRING CONTAINING THE TEXT WHICH IS A LINK YOU ARE INTERESTED IN.
-
 // YOU HAVE TO FETCH ALL THE LINK FROM A GIVEN PAGE WHICH CONTAINS THIS TEXT
 // javascript 
-
 // codewithharry.com 
 
-
-let links = documents.links;
-let filteredLink = '';
-
+let links = document.links;
 let filteredLink = [];
-
 for (let i = 0; i < links.length; i++) {
     if (links[i].href.includes('javascript')) {
         filteredLink.push(links[i].href);
