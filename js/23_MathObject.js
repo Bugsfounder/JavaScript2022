08aefe1120af752498eb9a225c4ccca5e21e7db0
Bugs Founder <bugsfounder2021@gmail.com>
2022-01-24 15:54:37 +0530

    Math Object In JavaScript | JavaScript Tutorial In Hindi #23

---
+++ js/23_MathObject.js
@@ -0,0 +1,45 @@
+console.log("JavaScript Math Object Tutorial");
+let x = 3;
+let y = 6;
+
+let z;
+z = x + y;
+z = x - y;
+z = x * y;
+z = x / y;
+
+
+// EXPLORING THE MATH OBJECT
+z = Math;
+z = Math.PI;
+z = Math.E;
+z = Math.round(5.5);
+z = Math.ceil(5.5);
+z = Math.floor(-5.1);
+z = Math.abs(-5); // NEGATIVE VALUES INTO POSITIVE VALUES
+z = Math.sqrt(49); // NEGATIVE VALUES INTO POSITIVE VALUES
+z = Math.pow(2, 4); // NEGATIVE VALUES INTO POSITIVE VALUES
+z = Math.min(2, 4, 23, 543, 23, 23, 23, 23, 23); // NEGATIVE VALUES INTO POSITIVE VALUES
+z = Math.max(2, 4, 23, 543, 23, 23, 23, 23, 23); // NEGATIVE VALUES INTO POSITIVE VALUES
+/* 
+a = (0,1)
+a100 = a*100 = (0, 100)
+a10_100 = 10+a*(100 - 10)
+*/
+
+z = Math.random() * 100; // RANDOM NUMBERS BETWEEN 0 TO 100
+z = Math.round(Math.random() * (100 - 50) + 50);
+
+console.log(z);
+
+
+// 3
+// 2.8
+// 2
+// 1
+// 0
+// -1
+// -2
+// -2.7
+// -3
+// -4
